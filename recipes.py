b23e76c4aee6e507b395cc0aef8ddc766c7f52e9
avkhimen <avkhimen@ualberta.ca>
2022-01-03 15:50:00 -0700

    adding more recipes

--- recipes.py
+++ recipes.py
@@ -4,5 +4,6 @@ recipes = {}
 recipes['potatoes_and_meat'] = {'potatoes' : 500, 'meat' : 300}
 recipes['eggs_and_carrots'] = {'eggs' : 120, 'carrots' : 200}
 recipes['bacon_and_eggs'] = {'eggs' : 120, 'bacon' : 50}
+recipes['potatoes_and_eggs'] = {'eggs' : 120, 'potatoes' : 150}
 
 RECIPES = recipes
